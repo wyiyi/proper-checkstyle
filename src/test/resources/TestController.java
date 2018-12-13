6ef70ec7ee8876bb50b6a6f85517521928d19797
Alpha Hinex <AlphaHinex@gmail.com>
2018-12-13 15:11:23 +0800

    Add test resource

---
+++ src/test/resources/TestController.java
@@ -0,0 +1,23 @@
+package com.proper.checkstyle;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TestController {
+
+    @ApiOperation("blah blah")
+    public String update(String id) {
+        return "";
+    }
+
+    @GetMapping
+    @ApiOperation("测试‍")
+    public String get(String ids) {
+        return "";
+    }
+
+    public String delete(String id) {
+        return "";
+    }
+
+}
