04029ce29134276723cafb9703cc686dbb197d39
Alpha Hinex <AlphaHinex@gmail.com>
2018-12-14 14:47:38 +0800

    Add checkers (#3)

--- src/main/java/com/proper/checkstyle/SwaggerCheck.java
+++ src/main/java/com/proper/checkstyle/SwaggerCheck.java
@@ -2,16 +2,10 @@ package com.proper.checkstyle;
 
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import com.puppycrawl.tools.checkstyle.api.FileContents;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class SwaggerCheck extends AbstractCheck {
     private static final String SWAGGER_ANNOTATION = "@ApiOperation";
-    private String anno = SWAGGER_ANNOTATION;
-
-    public void setAnno(String anno) {
-        this.anno = anno;
-    }
 
     @Override
     public int[] getDefaultTokens() {
@@ -20,21 +14,7 @@ public class SwaggerCheck extends AbstractCheck {
 
     @Override
     public void visitToken(DetailAST ast) {
-        FileContents fileContents = getFileContents();
-        String str = fileContents.getFileName();
-        if (str.endsWith("Controller.java")) {
-            if (ast.branchContains(TokenTypes.METHOD_DEF)) {
-                if (ast.branchContains(TokenTypes.ANNOTATIONS)) {
-                    if (String.valueOf(TokenTypes.ANNOTATIONS).contains(anno)) {
-                        return;
-                    }
-                } else {
-                    String message = "Failed！The methods no have swagger annotation [" + ast.getText() + "]";
-                    log(ast.getLineNo(), "HINEX_KEY_TEST");
-                    log(ast.getLineNo(), message);
-                }
-            }
-        }
+        // do sth.
     }
 
     @Override
