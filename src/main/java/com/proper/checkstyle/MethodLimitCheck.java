141fdc2d9ff6505661b32fe3c7ffdbda2c674aab
wyiyi <1084896032@qq.com>
2018-12-15 07:10:08 +0800

    [完成][master][pep-1592] method limit && swagger annotation (#4)

---
+++ src/main/java/com/proper/checkstyle/MethodLimitCheck.java
@@ -0,0 +1,44 @@
+package com.proper.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class MethodLimitCheck extends AbstractCheck {
+    private int max = Instance.DEFAULT_MAX;
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public int[] getDefaultTokens() {
+        return new int[]{TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF};
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        // find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
+        DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+
+        // count the number of direct children of the OBJBLOCK
+        // that are METHOD_DEFS
+        int methodDefs = objBlock.getChildCount(TokenTypes.METHOD_DEF);
+
+        // report error if limit is reached
+        if (methodDefs > this.max) {
+            String message = "too many methods, only " + this.max + " are allowed";
+            log(ast.getLineNo(), message);
+        }
+    }
+
+    @Override
+    public int[] getAcceptableTokens() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] getRequiredTokens() {
+        return new int[0];
+    }
+}
--- src/main/java/com/proper/checkstyle/SwaggerCheck.java
+++ src/main/java/com/proper/checkstyle/SwaggerCheck.java
@@ -2,19 +2,41 @@ package com.proper.checkstyle;
 
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.AnnotationUtil;
 
 public class SwaggerCheck extends AbstractCheck {
-    private static final String SWAGGER_ANNOTATION = "@ApiOperation";
+
+    private String anno = Instance.SWAGGER_ANNOTATION;
+
+    private String filter = Instance.FILTER;
+
+    public void setAnno(String anno) {
+        this.anno = anno;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
 
     @Override
     public int[] getDefaultTokens() {
-        return new int[]{TokenTypes.METHOD_DEF, TokenTypes.ANNOTATION, TokenTypes.IDENT};
+        return new int[]{TokenTypes.METHOD_DEF};
     }
 
     @Override
     public void visitToken(DetailAST ast) {
-        // do sth.
+        FileContents fileContents = getFileContents();
+        String str = fileContents.getFileName();
+        if (str.endsWith(filter)) {
+            if (AnnotationUtil.containsAnnotation(ast, anno)) {
+                return;
+            } else {
+                String message = "Failed！The methods no have swagger annotation [" + ast.getText() + "]";
+                log(ast.getLineNo(), message);
+            }
+        }
     }
 
     @Override
