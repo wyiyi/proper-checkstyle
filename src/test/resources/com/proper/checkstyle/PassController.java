64750fe83b1025f74f16c5a1255d95acfb185478
wyiyi <1084896032@qq.com>
2018-12-24 16:37:30 +0800

    1

--- src/test/resources/com/proper/checkstyle/PassController.java
+++ src/test/resources/com/proper/checkstyle/PassController.java
@@ -50,34 +50,12 @@ public class PassController {
         @ApiModelProperty(name = "‍编码", required = true)
         private String code;
 
-        @ApiModelProperty(name = "‍名称", required = true)
-        private String typeName;
-
-        @ApiModelProperty(name = "‍应用顺序", required = true)
-        private String sort;
-
         public String getCode() {
             return code;
         }
 
         public void setCode(String code) {
             this.code = code;
-        }
-
-        public String getTypeName() {
-            return typeName;
-        }
-
-        public void setTypeName(String typeName) {
-            this.typeName = typeName;
-        }
-
-        public String getSort() {
-            return sort;
-        }
-
-        public void setSort(String sort) {
-            this.sort = sort;
         }
 
         @Override
