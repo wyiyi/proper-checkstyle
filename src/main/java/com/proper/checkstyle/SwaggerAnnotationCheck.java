e0680bec13da529c322c8783ef90839203b7d871
Alpha Hinex <AlphaHinex@gmail.com>
2019-01-08 14:54:20 +0800

    Refactor

--- src/main/java/com/proper/checkstyle/SwaggerAnnotationCheck.java
+++ src/main/java/com/proper/checkstyle/SwaggerAnnotationCheck.java
@@ -46,6 +46,26 @@ public class SwaggerAnnotationCheck extends AbstractCheck {
         }
     }
 
+    /**
+     * `--OBJBLOCK -> OBJBLOCK [14:34]
+     *     |--LCURLY -> { [14:34]
+     *     |--METHOD_DEF -> METHOD_DEF [16:4]
+     *     |   |--MODIFIERS -> MODIFIERS [16:4]
+     *     |   |   |--ANNOTATION -> ANNOTATION [16:4]
+     *     |   |   |   |--AT -> @ [16:4]
+     *     |   |   |   |--IDENT -> GetMapping [16:5]
+     *     |   |   |   |--LPAREN -> ( [16:15]
+     *     |   |   |   |--EXPR -> EXPR [16:16]
+     *     |   |   |   |   `--STRING_LITERAL -> "/latest" [16:16]
+     *     |   |   |   `--RPAREN -> ) [16:25]
+     *     |   |   |--ANNOTATION -> ANNOTATION [17:4]
+     *     |   |   |   |--AT -> @ [17:4]
+     *     |   |   |   |--IDENT -> ApiOperation [17:5]
+     *     |   |   |   |--LPAREN -> ( [17:17]
+     *     |   |   |   |--EXPR -> EXPR [17:18]
+     *     |   |   |   |   `--STRING_LITERAL ->
+     * @param ast
+     */
     private void checkMethods(DetailAST ast) {
         // 通过 CLASS_DEF 的树节点获取每一个 METHOD_DEF
         DetailAST firstMethod = ast.findFirstToken(TokenTypes.OBJBLOCK).findFirstToken(TokenTypes.METHOD_DEF);
@@ -55,19 +75,15 @@ public class SwaggerAnnotationCheck extends AbstractCheck {
         }
     }
 
-    private void checkSingleMethod(DetailAST ast) {
-        if (!ast.branchContains(TokenTypes.ANNOTATION)) {
+    private void checkSingleMethod(DetailAST method) {
+        if (!method.branchContains(TokenTypes.ANNOTATION)) {
             return;
         }
         boolean isRequestMapping = false;
         boolean hasSwaggerAnnotation = false;
-        DetailAST firstAnnotation = AnnotationUtil.getAnnotationHolder(ast).getFirstChild();
-        for (DetailAST child = firstAnnotation; child != null; child = child.getNextSibling()) {
-            final DetailAST detailAST = child.getFirstChild();
-            if (detailAST == null) {
-                continue;
-            }
-            final String annotationName = FullIdent.createFullIdent(detailAST.getNextSibling()).getText();
+        DetailAST firstAnnotation = AnnotationUtil.getAnnotationHolder(method).getFirstChild();
+        for (DetailAST anno = firstAnnotation; anno != null && anno.getType() == TokenTypes.ANNOTATION; anno = anno.getNextSibling()) {
+            String annotationName = FullIdent.createFullIdent(anno.getFirstChild().getNextSibling()).getText();
             if (annotationName.endsWith(REQUEST_MAPPING_SUFFIX)) {
                 isRequestMapping = true;
             } else if (annotationName.equals(ANNOTATION_SWAGGER_OPERATION)) {
@@ -76,7 +92,7 @@ public class SwaggerAnnotationCheck extends AbstractCheck {
         }
         if (isRequestMapping && !hasSwaggerAnnotation) {
             String message = "There must be swagger annotation '@ApiOperation'on the method!";
-            log(ast.getLineNo(), message);
+            log(method.getLineNo(), message);
         }
     }
 
